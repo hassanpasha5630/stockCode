95ef2c56af96d24162600780a389d0e03767c566
hassanpasha5630 <Hassanpasha.pe@gmail.com>
2017-01-07 03:28:36 -0600

    Update Scrapy.py
    duplicates

--- Scrapy.py
+++ Scrapy.py
@@ -26,44 +26,88 @@ while True:
             soup = BeautifulSoup(page,"html.parser")
             try:
                 Stock_Name = soup.find("h1", attrs= {"class":"D(ib) Fz(18px)"}).text.strip()
+import re
+from bs4 import BeautifulSoup
+import urllib
+import urllib.request
+import csv
+import time
+from datetime import datetime
+
+
+URL1 = "https://finance.yahoo.com/quote/tmus?ltr=1"
+URL2 = "https://finance.yahoo.com/quote/TWTR?p=TWTR"
+URL3 = "https://finance.yahoo.com/quote/aapl?ltr=1"
+URL4 = "https://finance.yahoo.com/quote/AMDA?p=AMDA"
+
+Links =[URL1,URL2,URL3,URL4]
+Data = []
+TypeCast = []
+
+while True:
+    now = datetime.now().strftime('%H%M')
+    if '0830' <= now <= '0300' or '0830' <= now <= '1500' or '0200' <= now <= '0400' :
+        for stocks in Links :
+
+
+            page = urllib.request.urlopen(stocks)
+            soup = BeautifulSoup(page,"html.parser")
+            try:
+                Stock_Name = soup.find("h1", attrs= {"class":"D(ib) Fz(18px)"}).text.strip()
                 print(Stock_Name)
+
 
             except:
                 Stock_Name = soup.find('div', {"class": "D(ib)"}).find('h1').text.strip()
                 print(Stock_Name)
+
           #      print("        ")
             try:
                 Stock_Price = soup.find("span", attrs={"class":"Fw(b) Fz(36px) Mb(-4px)"}).text.strip()
                 print(Stock_Price)
                 typecasting = float(Stock_Price)
                 TypeCast.append(typecasting)
+
            #     print("        ")
             except:
                 Stock_Price = soup.find('div',{"class":"D(ib) Fw(200) Mend(20px)"}).find('span',{"class": " Fw(b) Fz(36px) Mb(-4px)"}).text
                 print(Stock_Price)
                 typecasting = float(Stock_Price)
                 TypeCast.append(typecasting)
+
             #    print("        ")
             try :
                 Percentage = soup.find("span", attrs={"class":"Fw(500) Pstart(10px) Fz(24px) C($dataRed)"}).text.strip()
                 print(Percentage)
                 print("Red")
+
              #   print("        ")
             except:
                 Percentage =soup.find("span", attrs={"class":"Fw(500) Pstart(10px) Fz(24px) C($dataGreen)"}).text.strip()
                 print(Percentage)
                 print("Green")
+
+            Data.append((Stock_Name, Stock_Price, Percentage))
+            with open('MyStock4.csv', 'a') as csv_file:
+                
+
+                write = csv.writer(csv_file)
+                for Stock_Name, Stock_Price, Percentage in Data:
+                    # time2 = datetime.now()
+                    print(Data)
+                    write.writerow([Stock_Name, Stock_Price, Percentage, datetime.now().strftime("%H:%M:%S")])
+
+
+
               #  print("        ")
 
-            Data.append((Stock_Name, Stock_Price ,Percentage))
-            with open('MyStock.csv', 'a') as csv_file:
-                write = csv.writer(csv_file)
-                for Stock_Name, Stock_Price , Percentage in Data:
-                    time2 = datetime.now()
-                    write.writerow([Stock_Name, Stock_Price,Percentage, datetime.strftime(time2,"%H%M%S")])
+
     else:
         print("Market is closed")
         break
+
+
+
 
    # typecating = int(Stock_Price)
    #TypeCast.append(typecating)
@@ -97,5 +141,5 @@ while True:
         print("AMDA stock is going down",TypeCast[3])
         print("        ")
 
-    print("sleeping for 30 sec")
-    time.sleep(30)
+    print("sleeping for 5 sec")
+    #time.sleep(30)
